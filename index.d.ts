fbe15c8eaa58570f02ede33f2d27687e29b20a04
Kasper Zutterman <kasperzutterman1999@gmail.com>
2021-10-15 11:31:21 +0200

    bump: bump version

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,2 @@
 export * from './lib/data-model';
+export * from './lib/Bucketizer';
--- index.js
+++ index.js
@@ -11,3 +11,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 exports.__esModule = true;
 __exportStar(require("./lib/data-model"), exports);
+__exportStar(require("./lib/Bucketizer"), exports);
