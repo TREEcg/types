ce85ced924a7f186aaffee657dd8704a4e60d0eb
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2021-11-23 10:53:01 +0100

    Update Bucketizer.ts
    - bucketize function is moved to the abstract class with, but can be overridden in subclasses
    
    - bucketize function now has a fallback function if a property path is not found and/or there is an error while creating the buckets

--- lib/Bucketizer.ts
+++ lib/Bucketizer.ts
@@ -1,7 +1,7 @@
 import type * as RDF from '@rdfjs/types';
 import { findNodes } from 'clownface-shacl-path';
 import { DataFactory } from 'rdf-data-factory';
-import { BucketizerOptions } from '..';
+import { BucketizerOptions, RelationType } from '..';
 import { RelationParameters } from './RelationParameters';
 const { dataset } = require('@rdfjs/dataset');
 const clownface = require('clownface');
@@ -11,11 +11,18 @@ export abstract class Bucketizer {
   public readonly factory: RDF.DataFactory;
   public propertyPathQuads: RDF.Quad[];
   private bucketHypermediaControlsMap: Map<string, RelationParameters[]>;
+  private bucketlessPageNumber: number;
+  private bucketlessPageMemberCounter: number;
+  public bucketizerOptions: BucketizerOptions;
 
-  public constructor() {
+  public constructor(bucketizerOptions: BucketizerOptions) {
     this.factory = new DataFactory();
     this.bucketHypermediaControlsMap = new Map<string, RelationParameters[]>();
+
     this.propertyPathQuads = [];
+    this.bucketlessPageNumber = 0;
+    this.bucketlessPageMemberCounter = 0;
+    this.bucketizerOptions = bucketizerOptions;
   }
 
   public setPropertyPathQuads = (propertyPath: string): Promise<void> => new Promise((resolve, reject) => {
@@ -39,7 +46,31 @@ export abstract class Bucketizer {
    * Adds extra triples to the array of quads indicating
    * the buckets in which the version object must be placed
    */
-  public abstract bucketize: (quads: RDF.Quad[], memberId: string) => void;
+  public bucketize = (quads: RDF.Quad[], memberId: string): void => {
+    const propertyPathObjects: RDF.Term[] = this.extractPropertyPathObject(quads, memberId);
+
+    if (propertyPathObjects.length <= 0) {
+      console.error(`[Bucketizer]: No matches found for property path "${this.bucketizerOptions.propertyPath}" in member "${memberId}". Applying fallback.`);
+
+      const bucketTriple = this.fallback(memberId);
+      quads.push(bucketTriple);
+
+      return;
+    }
+
+    let bucketTriples: RDF.Quad[] = [];
+    try {
+      const buckets = this.createBuckets(propertyPathObjects);
+      bucketTriples.push(...buckets.map(bucket => this.createBucketTriple(bucket, memberId)));
+
+    } catch (error) {
+      console.error(`[Bucketizer]: Error while creating the buckets for member ${memberId}. Applying fallback.`)
+      bucketTriples.push(this.fallback(memberId));
+
+    }
+
+    quads.push(...bucketTriples);
+  };
 
   /**
    * Selects the bucket for the LDES member based on the value of the property path object
@@ -81,12 +112,38 @@ export abstract class Bucketizer {
   public exportState(): any {
     return {
       hypermediaControls: Array.from(this.bucketHypermediaControlsMap.entries()),
-      propertyPathQuads: this.propertyPathQuads
+      propertyPathQuads: this.propertyPathQuads,
+      bucketizerOptions: this.bucketizerOptions,
+      bucketlessPageNumber: this.bucketlessPageNumber,
+      bucketlessPageMemberCounter: this.bucketlessPageMemberCounter
     }
   }
 
   public importState(state: any): void {
     this.bucketHypermediaControlsMap = new Map(state.hypermediaControls);
     this.propertyPathQuads = state.propertyPathQuads;
+    this.bucketizerOptions = state.bucketizerOptions;
+    this.bucketlessPageNumber = state.bucketlessPageNumber;
+    this.bucketlessPageMemberCounter = state.bucketlessPageMemberCounter;
+  }
+
+  public fallback = (memberId: string): RDF.Quad => {
+    const pageSize = this.bucketizerOptions.pageSize;
+
+    if (pageSize && this.bucketlessPageMemberCounter === pageSize) {
+      const currentPage = this.bucketlessPageNumber;
+
+      this.bucketlessPageNumber++;
+      this.bucketlessPageMemberCounter = 0;
+
+      const relationParameters: RelationParameters = {
+        nodeId: `bucketless-${this.bucketlessPageNumber}`,
+        type: RelationType.Relation
+      }
+      this.addHypermediaControls(`bucketless-${currentPage}`, [relationParameters]);
+    }
+
+    this.bucketlessPageMemberCounter++;
+    return this.createBucketTriple(`bucketless-${this.bucketlessPageNumber}`, memberId);
   }
 }
