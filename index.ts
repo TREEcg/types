3ecc74fc4965b59e52142a003e3f436024bc19ad
ajuvercr <arthur.vercruysse@ugent.be>
2023-05-08 15:35:25 +0200

    nothing logs so we don't need loggers

--- index.ts
+++ index.ts
@@ -3,7 +3,4 @@ export * from './lib/BucketizerOptions';
 export * from './lib/Fragment';
 export * from './lib/Member';
 export * from './lib/RelationParameters';
-export * from './lib/utils/Logger';
-export * from './lib/utils/Logger-Browser';
 export * from './lib/Vocabularies';
-
